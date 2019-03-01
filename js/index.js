2e610d45eeb42b86a45e1f6f2c3dde512a9724fd
Mohamed khaloti <simooo@Mohameds-MacBook-Pro.local>
2019-02-28 21:29:09 -0500

    Working on adding an event listener on button item

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // JS goes here
+class NavButton {
+    constructor(element){
+        this.element = element;
+        this.navItems = document.querySelector(".nav-tags");
+        console.log(this.navItems)
+    }
+}
+
+
+const menuButton = document.querySelectorAll(".navigation-button").forEach(button => new NavButton(button));
