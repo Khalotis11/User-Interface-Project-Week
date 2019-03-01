9513370e5c1d832c33ea675e1fc71c0b03a47078
Mohamed khaloti <simooo@Mohameds-MacBook-Pro.local>
2019-02-28 22:32:47 -0500

    Completed adding event listener on dropdown button

--- js/index.js
+++ js/index.js
@@ -3,9 +3,13 @@ class NavButton {
     constructor(element){
         this.element = element;
         this.navItems = document.querySelector(".nav-tags");
-        console.log(this.navItems)
+        // console.log(this.navItems)
+        this.element.addEventListener('click', () => this.handleClick());
+    }
+    handleClick() {
+        document.querySelectorAll('.navigation-button').forEach(element => element.classList.toggle('active'));
+        this.navItems.classList.toggle('hidden')
     }
 }
-
 
 const menuButton = document.querySelectorAll(".navigation-button").forEach(button => new NavButton(button));
