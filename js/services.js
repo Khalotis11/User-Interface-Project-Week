4450f16638cac9f9f47ae3517b4c42034b51186b
Mohamed khaloti <simooo@Mohameds-MacBook-Pro.local>
2019-03-04 22:01:18 -0500

    MVP

--- js/services.js
+++ js/services.js
@@ -1,28 +1,28 @@
 // Step 3: Create TabsLinks class definition and pass in the link reference to the DOM element
 class TabsLinks {
-    constructor(tabItems){
-        // console.log(tabItems)
-        this.tabItems = tabItems;
+    constructor(tabLink){
+        // console.log(tabLink);
+        this.tabLinks = tabLink;
          // Step 4: Match the data-set values between our tabs-link and the content data attribute from tabs-item.
-        this.tabContent = document.querySelector(`.tabs-item[data-tab='${this.tabItems.dataset.tab}']`);
+        this.tabContent = document.querySelector(`.tabs-item[data-tab='${this.tabLinks.dataset.tab}']`);
          // Step 5: Create a new content object from our matched data-set content tab
         this.tabContent = new Content(this.tabContent);
         // Step 8: Add event Listener for our link and pass in the implicit binding of the 'this' keyword using an arrow function
-        this.tabItems.addEventListener('click', () => this.linkClick());
+        this.tabLinks.addEventListener('click', () => this.linkClick());
     } 
     // methods
     // Step 10: Have the tabContent toggle off and on based on our Content class's method 
     linkClick() {
         const tabs = document.querySelectorAll('.tabs-link')
         tabs.forEach(tab => tab.classList.remove('selected-link'))
-        this.tabItems.classList.add('selected-link')
+        this.tabLinks.classList.add('selected-link')
         this.tabContent.toggleContent();
      }
  }
  // Step 6: Create Content class definition and pass in the tabContent reference to the DOM element
  class Content {
     constructor(tabElement) {
-    //   console.log(tabElement);
+      // console.log(tabElement);
       // Step 7: Properly set up the 'this' keyword for the Content class using our tabElement
       this.tabElement = tabElement;
     }
